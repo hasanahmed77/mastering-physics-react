74ac772275da780180cf5c3ced9454ba281d2e11
Hasan Ahmed <megatron.kyuubi@gmail.com>
2021-09-13 01:16:42 +0600

    completed the home page design

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Flex, Text, Button, Image } from '@chakra-ui/react';
 import './App.css';
+import Home from './components/Home';
 import Navbar from './components/Navbar';
 import MainImage from './images/sleep.jpg';
 
@@ -10,79 +11,11 @@ function App() {
       flexDir="column"
       overflow="hidden"
       maxW="2000 px"
-      ml="13rem"
-      mr="13rem"
+      pl="10%"
+      pr="10%"
     >
       <Navbar/>
-
-      {/* Column 2 */}
-      <Flex
-        mt="3.5rem"
-        flexDir="row"
-        justifyContent="space-between"
-        maxHeight="45%"
-      >
-        {/* Texts */}
-        <Flex
-          max
-          flexDir="column"
-          justifyContent="space-between"      
-        > 
-          <Flex
-            flexDir="column"
-            justifyContent="space-between"
-            fontSize="6xl"
-            className="font-bold"     
-          >
-            <Text>To Learn,</Text>
-            <Text mt="-3.5">one must solve</Text>
-          </Flex>
-          <Flex
-            fontSize="2xl"
-            className="font-light"
-            flexDir="column"
-            mt="-3"
-          >
-            <Text>
-              Gamify the process of learning
-              by 
-            </Text>
-            <Text>
-              replacing lecture videos with
-            </Text>
-            <Text>     
-              interactive lessons!
-            </Text>
-          </Flex>
-          <Flex>
-            <Button
-             size="lg" 
-             className="font-medium"
-             height="4rem"
-             fontWeight="medium"
-            >
-               Get Started
-            </Button>
-          </Flex>
-        </Flex>
-        <Flex
-
-        > {/* Picture */}
-          <Image
-            flexDir="row"
-            justifyContent="flex-end"
-            width="100%"
-            height="100%" 
-            src={MainImage} 
-            alt="I Sleep"
-          />
-        </Flex>
-      </Flex>
-
-      {/* Column 3 */}
-      <Flex>
-
-      </Flex>
+      <Home/>
     </Flex>
   );
 }
